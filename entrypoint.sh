f483d7c83067e2809f7a8f43beab2fbbc1230107
s.sirigiredd <s.sirigiredd@partner.samsung.com>
2023-01-18 17:09:06 -0800

    echo args

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ echo "${KUBE_CONFIG_DATA}" | base64 -d > kubeconfig
 export KUBECONFIG=kubeconfig
 for i in "$@"
 do
+  echo ${i}
   result="$(kubectl i)"
   status=$?
   if [[ $status -eq 0 ]]; then continue ; else exit 1; fi
