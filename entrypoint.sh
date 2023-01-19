1dfe6058584537d388c4891b3c28516996291aff
s.sirigiredd <s.sirigiredd@partner.samsung.com>
2023-01-18 17:12:00 -0800

    correcttted type in args

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ export KUBECONFIG=kubeconfig
 for i in "$@"
 do
   echo ${i}
-  result="$(kubectl i)"
+  result="$(kubectl ${i})"
   status=$?
   if [[ $status -eq 0 ]]; then continue ; else exit 1; fi
 done
